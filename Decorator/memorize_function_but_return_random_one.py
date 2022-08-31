86ef3b227645720215b8912275014da9fc5cc618
Marc Grziwa <marc.grziwa@perfact.de>
2022-08-31 14:59:34 +0200

    Threw it all in the garbage and rebuild it. Works fine now

--- Decorator/memorize_function_but_return_random_one.py
+++ Decorator/memorize_function_but_return_random_one.py
@@ -1,35 +1,68 @@
-def num(num):
-    max_number = {}
-
-    def name(a_func):
-        def randomizer():
-            numb = num
-            x = 0
-            while 0 < numb:
-                max_number[x] = x
-                x += 1
-                numb -= 1
-            x = 0
-            for i in max_number:
-                x += 1
-                return max_number[x]
-            print(max_number)
-            print(max_number[0])
-            print(max_number[1])
-            print(max_number[2])
-            print(max_number[3])
-            a_func()
-        return randomizer
-    return name
+import random
 
 
-@num(5)
-def printer():
-    print("Hallo")
+def name(a_func):
+    results = []
+
+    def randomizer(showlist=False):
+        if "Nice" not in results and "Quatsch" not in results:
+            results.append("Nice")
+        else:
+            results.append("Quatsch")
+        a_func(random.choice(results))
+        if showlist:
+            print(results)
+    return randomizer
+
+
+@name
+def printer(results):
+    print(results)
+    return
 
 
 def main():
     printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer()
+    printer(showlist=True)
 
 
 if __name__ == "__main__":
